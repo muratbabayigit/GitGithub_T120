5c2793b7004daff46c2d4de6a8417b70db8413b5
Murat Babayiğit <babayigit@gmail.com>
2023-07-29 18:42:16 +0300

    20230729.v1.Murat

---
+++ src/Hello_Git.java
@@ -0,0 +1,9 @@
+public class Hello_Git {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+        // git add content root path ile sadece content root adresi girilen dosyayı gönderir.
+        // git add .    Tüm dosyaları stage area atar
+    }
+}
+
